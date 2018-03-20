3d18366bd2c23deac36431c0933cc6170102ed7e
Adam Storr <astorr@gmail.com>
2018-03-20 19:21:32 +0000

    Add in verify and allow for posting through to controller action

--- samples/FitbitCoreReceiver/Controllers/FitbitController.cs
+++ samples/FitbitCoreReceiver/Controllers/FitbitController.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.WebHooks;
 using Newtonsoft.Json.Linq;
 
@@ -10,7 +7,13 @@ namespace FitbitCoreReceiver.Controllers
     public class FitbitController
     {
         [FitbitWebHook]
-        public IAsyncResult FitbitScription(string id, JObject data)
+        public IAsyncResult FitbitSubscription(string id, JArray data)
+        {
+            return null;
+        }
+
+        [FitbitWebHook(Id="my_id")]
+        public IAsyncResult FitbitSubscriptionId(JArray data)
         {
             return null;
         }
