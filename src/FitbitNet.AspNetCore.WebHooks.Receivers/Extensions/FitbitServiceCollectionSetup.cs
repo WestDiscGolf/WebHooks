d5a1e86b85fcd88f387c9872e56aea5816d486b9
Adam Storr <astorr@gmail.com>
2018-03-20 09:27:53 +0000

    Add in the basic structure of project

---
+++ src/FitbitNet.AspNetCore.WebHooks.Receivers/Extensions/FitbitServiceCollectionSetup.cs
@@ -0,0 +1,13 @@
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal static class FitbitServiceCollectionSetup
+    {
+        public static void AddFitbitServices(IServiceCollection services)
+        {
+
+        }
+    }
+}
